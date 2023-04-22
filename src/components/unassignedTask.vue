10c2ae38f1fc2f2d2488f73caa439998c4b32f9b
Peter Bikundo <binmonk@gmail.com>
2023-04-22 18:51:04 +0300

    Update unassignedTask.vue

--- src/components/unassignedTask.vue
+++ src/components/unassignedTask.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-2 p-2 border-2 rounded">
-    <h5 class="mb-2  font-bold tracking-tight text-gray-900 dark:text-white">
+    <h5 class="mb-2  font-bold tracking-tight text-gray-900 dark:text-white text-red-800">
       <small>
         task is not assigned, assign it to a user below
       </small>
